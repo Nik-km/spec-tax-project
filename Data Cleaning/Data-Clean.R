2b327e9ea192c7896bd25fecb25335580f66c73b
Nik-km <nikolova.km@gmail.com>
2022-02-16 17:11:47 -0500

    Commit from RStudio

---
+++ Data Cleaning/Data-Clean.R
@@ -0,0 +1,143 @@
+
+setwd("~/Projects/spec-tax-project/Data Cleaning")
+
+library(knitr)      # # for `kable()`
+library(tidyverse)  # attaches the packages: ggplot2, tibble, tidyr, readr, purrr, dplyr, stringr, & forcats
+library(readxl)
+library(XML)
+library(xml2)
+library(haven)      # reads .sav files
+library(lubridate)  # formats dates
+
+
+library(lmtest)
+library(sandwich)   # fn. for coeftest
+library(plm)        # R function that can estimate linear panel regression models
+library(stargazer)
+library(dynlm)      # Time-series regression
+
+
+
+##################################
+###     Labour Force Survey     ##
+##################################
+
+# Create a large list of all the datasets
+setwd("~/Projects/spec-tax-project/Data Cleaning/LFS_clean_data")
+LFS.2021.df <- list() # creates an empty list
+listsav <- dir(pattern = "*.sav") # creates the list of all the csv files in the directory
+for (k in 1:length(listsav)){
+    LFS.2021.df[[k]] <- read_sav(listsav[k]) # str(LFS.df[[1]])
+}
+
+LFS.2021.df <- data.table::rbindlist(LFS.2021.df)   # merge the list of tibbles into 1 data frame
+
+# Prepping the full year dataset
+PROV_ex <- c(10, 11, 12, 13, 46, 47)  # excluded provinces
+CMA_ex <- c(1,6)  # excluded CMAs
+LFS.2021.df <- LFS.2021.df %>%   # de-merge into list of tibbles again
+    group_by(SURVMNTH, .add = TRUE) %>% group_split() %>% 
+    map(. %>% 
+            dplyr::filter(!(PROV %in% PROV_ex)) %>% 
+            dplyr::filter(!(CMA %in% CMA_ex)) %>% 
+            mutate(DAY = 1) %>% 
+            mutate(DATE = make_date(SURVYEAR, SURVMNTH, DAY)) %>% 
+            dplyr::select("REC_NUM","DATE","SURVMNTH","LFSSTAT","PROV","CMA","AGE_12","SEX","MARSTAT",  # Keep the following columns
+                          "EDUC","FTPTLAST", "COWMAIN","IMMIG","NAICS_21","NOC_10",
+                          "HRLYEARN","UNION","PERMTEMP","ESTSIZE","FIRMSIZE","DURUNEMP",
+                          "UNEMFTPT","DURJLESS","SCHOOLN","EFAMTYPE","AGYOWNK"
+            )
+    )
+
+LFS.2021.df <- data.table::rbindlist(LFS.2021.df)   # final merge the list of tibbles
+
+colnames(LFS.2021.df)
+
+# saving dataset as a .csv file
+write.csv(LFS.2021.df, "C:\\Users\\M\\Projects\\spec-tax-project\\LFS Data\\LFS-2021.csv", row.names = FALSE)
+
+
+
+
+
+
+##################################
+###          CANSIM             ##
+##################################
+library(cansim)
+
+?get_cansim_table_list
+avail.data <- list_cansim_cubes()
+
+
+# Retrieve "New housing price index, monthly": 18-10-0205-01 (formerly: CANSIM 327-0056)
+get_cansim_table_overview("18-10-0205", language = "english", refresh = FALSE)
+
+NHPI <- get_cansim("18-10-0205")    # Spans 1981-01-01 to 2021-12-01,   Index: 2016-12=100
+
+# Regions to include
+CMA_ID <- sort(c(550, 537, 541, 532, 539, 535, 915, 933, 935,                   # treatments
+                 825, 835, 462, 421, 580, 555, 559, 35505, 24505,59,35,24))     # controls & BC, ON, QB
+
+NHPI <- NHPI %>% 
+    dplyr::rename(Estate_Type = "New housing price indexes", 
+                  Type_Hierarchy = "Hierarchy for New housing price indexes",
+                  GEO_Hierarchy = "Hierarchy for GEO") %>% 
+    dplyr::select(Date, GEO, GeoUID, "VECTOR", Estate_Type, VALUE, STATUS, val_norm, 
+                  Type_Hierarchy, GEO_Hierarchy) %>% 
+    dplyr::mutate(Date = ymd(Date)) %>% 
+    dplyr::filter(Date >= "2013-01-01" & Date <= "2021-12-01") %>% 
+    dplyr::filter(GeoUID %in% CMA_ID) %>% 
+    dplyr::mutate(GEO = as.factor(GEO), GeoUID = as.factor(GeoUID))
+
+
+levels(NHPI$GEO)
+levels(NHPI$GeoUID)
+
+### GeoUID Markers ###
+# Guelph: 550     Hamilton: 537     Kitchener-Cambridge-Waterloo: 541       Oshawa: 532          St. Catharines-Niagara: 539       Toronto: 535
+# Kelowna: 915      Vancouver: 933      Victoria: 935
+
+### Controls:
+# Calgary: 825        Edmonton: 835        Montreal: 462       Quebec City: 421
+# Greater Sudbury: 580        London: 555       Windsor: 559        Ottawa-Gatineau (ON part): 35505        Ottawa-Gatineau (QB part): 24505
+
+
+# NOTE: Sudbury & London are a strong contenders as control regions for comparing the NRST
+
+
+
+##################################
+###     Housing Prices Index   ###
+##################################
+
+
+
+TO.HP.df <- NHPI %>% 
+    dplyr::filter(VECTOR == "v111955500")
+
+
+
+
+
+
+
+
+
+##################################
+###         Rough Work          ##
+##################################
+
+PROV_ID <- c(59, 35, 24)  # BC, ON, QB
+
+# Making the province and territory names consistent
+can.df <- can.df %>%
+    dplyr::mutate(
+        Province_Territory = gsub(", Canada", "", Province_Territory) %>% factor(),
+        Province_Territory = gsub(",Canada", "", Province_Territory) %>% factor()
+    )
+
+
+
+
+
